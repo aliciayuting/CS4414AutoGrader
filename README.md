40e33a83357f664ea3cd4e51c1ba7fbc3637945c
aliciayuting <yy354@cornell.edu>
2023-01-30 22:56:04 -0500

    auto grader for hw1

---
+++ README.md
@@ -0,0 +1,38 @@
+# hw1 grading direction
+
+## 1. How to use autograder:
+- download a batch of students' submitted assignment from cms.
+     - under hw1 group 
+     - select the students' netids of your grading group
+     - then click Files on the top right corner of the page, under [Select Group Operations:]
+- scp this folder to your ugclinux server
+- scp the autograder script folder to your ugclinux server as well
+- all the four files should sit under the directory of Submissions 
+     Your directories should look as follows:
+     - Submissions
+     ----- hw1_edge.txt
+     ----- hw1_norm.txt
+     ----- hw1grader.py
+     ----- standard_output.py
+     ----- netid_1
+     ---------- 'my screenshot.pdf'
+     ---------- 'My source file.cpp'
+     ----- netid_2
+     ---------- 'my screenshot.pdf'
+     ---------- 'My source file.cpp'
+     ....
+
+- run the autograder by calling `python hw1grader.py`
+- after running the autograder, there is a grades.csv generated with the score of each student's code
+     - grades == 3 means it passed
+     - grades == 0 means the student's code didn't compile. In this case, please double check if their code file exist, and if you can manually compile. If not, 0 is the score the student get
+     - grades == 1 means one of the test cases doesn't match. In this case, please double check with the grading rubric below, and assign score accordingly.
+
+
+## 2. Grading rubric
+
+-1 point if one of the following happen, do not double count each issue(since we have two test cases), max points off is -2:
+  - The printout full name is incorrect
+  - The letter counting is incorrect
+  - The 30 blank spaces before letter, and the 10 blank spaces after ':' is incorrect
+  - The ordering of A-Z, then A-z is incorrect
---
+++ hw1grader.py
@@ -0,0 +1,91 @@
+import os
+import subprocess
+import shutil
+from standard_output import *
+import csv
+
+OUTER_DIR= os.path.abspath(os.getcwd())
+
+graded_netids = []
+passed_netids = []
+unpassed_netids = []
+unpassed_netids_reasons = []
+
+fields = ['netid', 'pass/fail', 'reason']
+grades_file = []
+
+
+def checkoutput(netid, output, expected_list):
+     output_list = str(output.decode()).splitlines()
+     for i in range(len(expected_list)):
+          for j in range(len(expected_list[i])):
+               if(not expected_list[i][j] in output_list[i] ):
+                    graded_netids.append(netid)
+                    unpassed_netids.append(netid)
+                    reason = "output miss match\nExpected:"
+                    reason += expected_list[i]
+                    reason += "\nYour output:"
+                    reason += output_list[i]
+                    unpassed_netids_reasons.append(reason)
+                    grades_file.append([str(netid), "1", reason])
+                    return False
+     return True
+          
+
+
+def main():
+     subfolders = [ f.path for f in os.scandir(OUTER_DIR) if f.is_dir() ]
+     for x in subfolders:
+          netid = x[(x.rfind('/') + 1):]
+          if(netid == "__pycache__"):
+               continue
+          try:
+               print("- Grading: " + netid)
+               dir = os.path.join(OUTER_DIR, x)
+               mv_cmd = "mv 'My source file.cpp' hello.cpp"
+               ps = subprocess.Popen(mv_cmd,shell=True, stdout=subprocess.PIPE,stderr=subprocess.STDOUT, cwd=dir)
+               (output, err) = ps.communicate()  
+               p_status = ps.wait()
+
+               p = subprocess.check_output(["g++", "-std=c++2a", "hello.cpp", "-o", "hello"], cwd=dir) 
+               if(p):
+                    graded_netids.append(netid)
+                    unpassed_netids.append(netid)
+                    unpassed_netids_reasons.append("Program unable to compile")
+               cmd = "./hello < hw1_norm.txt"
+               shutil.copy(os.path.join(OUTER_DIR,"hw1_norm.txt"), x)
+               ps = subprocess.Popen(cmd,shell=True, stdout=subprocess.PIPE,stderr=subprocess.STDOUT, cwd=dir)
+               output = ps.communicate()[0]
+               check = checkoutput(netid, output, hw1_norm_standard_output)
+               if(not check):
+                    continue
+
+               cmd = "./hello < hw1_edge.txt"
+               shutil.copy(os.path.join(OUTER_DIR,"hw1_edge.txt"), x)
+               ps = subprocess.Popen(cmd,shell=True,stdout=subprocess.PIPE,stderr=subprocess.STDOUT, cwd=dir)
+               output = ps.communicate()[0]
+               check = checkoutput(netid, output, hw1_edge_standard_output)
+               if(not check):
+                    continue
+               passed_netids.append(netid)
+               grades_file.append([str(netid), "3", "Good work!"])
+          except:
+               graded_netids.append(netid)
+               unpassed_netids.append(netid)
+               unpassed_netids_reasons.append("Grader unable to run")
+               print("Grader unable to run on netid: " + netid)
+               grades_file.append([str(netid), "0", "Grader unable to run"])
+     print("-passed netids:", passed_netids)
+     print("-unpassed netids:",unpassed_netids)
+     print("-unpassed reasons: ",unpassed_netids_reasons)
+
+
+
+
+if __name__ == "__main__":
+     main()
+     with open('grades.csv', 'w') as f:  
+          # using csv.writer method from CSV package
+          write = csv.writer(f)     
+          write.writerow(fields)
+          write.writerows(grades_file)
